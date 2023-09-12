4f7f4db7a6d8547c6ed1404adba6453f73f9d520
Yifan SHI <41503744+AidenSaika@users.noreply.github.com>
2023-09-11 21:37:29 -0400

    Changes

---
+++ assignments/Assignment2_Basics/Assets/player.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    float speed = 0.5f;
+    float speed1 = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed1 = 2.5f;
+        }
+        if (Input.GetKey(KeyCode.LeftShift) == false)
+        {
+            speed1 = 1;
+        }
+
+        
+        if (transform.position.x > 5.2)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(Vector3.right * speed1 * Time.deltaTime);
+            }
+        }
+     
+         
+        if (Input.GetKey(KeyCode.DownArrow))  
+        {
+           transform.Translate(Vector3.left * speed1 * Time.deltaTime);
+        }  
+         
+        if (Input.GetKey(KeyCode.LeftArrow))  
+        {
+           transform.Translate(Vector3.back * speed1 * Time.deltaTime);
+        }  
+        
+        if (Input.GetKey(KeyCode.RightArrow))  
+        {
+           transform.Translate(Vector3.forward * speed1 * Time.deltaTime);
+        }  
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Rotate(Vector3.back, speed);
+        }
+        if (Input.GetKey(KeyCode.Space) == false)
+        {
+                transform.rotation = Quaternion.identity;
+                transform.Rotate(0, 0, -180);
+        }
+
+
+    }
+}
---
+++ assignments/Assignment2_Basics/Assets/prefab.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prefab : MonoBehaviour
+{
+    public GameObject TreePrefab;
+    public GameObject GressPrefab;
+    public GameObject CrystalPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 150; i++)
+        {
+            generateTree();
+        }
+        for (int j = 0; j < 200; j++)
+        {
+            generateGress();
+        }
+        for (int k = 0; k < 10; k++)
+        {
+            generateCrystal();
+        }
+    }
+    void generateTree()
+    {
+        float x = Random.Range(-4, 4.9f);
+        float y = 0;
+        float z;
+        double temp = Random.Range(0, 2);
+        if (temp == 0)
+            z = Random.Range(-10, -2);
+        else
+            z = Random.Range(2, 10);
+        Vector3 pos = new Vector3(x, y, z);
+        GameObject treeObj = Instantiate(TreePrefab, pos, Quaternion.identity);
+
+        float X = 0;
+        float Y = Random.Range(-180, 180);
+        float Z = 0;
+        Vector3 Rotate = new Vector3(X, Y, Z);
+        treeObj.transform.Rotate(Rotate);
+    }
+
+    void generateGress()
+    {
+        float Gx = Random.Range(16, 25);
+        float Gy = 0;
+        float Gz = Random.Range(-10,10);
+ 
+        Vector3 GressV = new Vector3(Gx, Gy, Gz);
+        GameObject GressObj = Instantiate(GressPrefab, GressV, Quaternion.identity);
+
+    
+    }
+    void generateCrystal()
+    {
+        float Cx = Random.Range(0, 1.3f);
+        float Cy = Random.Range(7.39f,13);
+        float Cz = Random.Range(-1.8f, 1.81f);
+
+        Vector3 CrystalV = new Vector3(Cx, Cy, Cz);
+        GameObject CrystalObj = Instantiate(CrystalPrefab, CrystalV, Quaternion.identity);
+    }
+        // Update is called once per frame
+        void Update()
+    {
+       
+
+    }
+}
+
+
+
+
+
+
+
+
+
