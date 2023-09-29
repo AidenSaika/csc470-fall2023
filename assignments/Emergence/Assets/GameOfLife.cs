09edda5acdec995798d85a8c9e0f58dfc8ef2d44
Yifan SHI <41503744+AidenSaika@users.noreply.github.com>
2023-09-28 22:45:33 -0400

    1

--- assignments/Emergence/Assets/GameOfLife.cs
+++ assignments/Emergence/Assets/GameOfLife.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class GameOfLife : MonoBehaviour
 {
     public GameObject cellPrefab;
-    cell[,] cells;
+    public cell[,] cells;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +20,13 @@ public class GameOfLife : MonoBehaviour
                 Vector3 pos = transform.position;
                 pos.x = pos.x + x * (1 + 0.1f);
                 pos.z = pos.z + y * (1 + 0.1f);
+                pos.y = 1;
                 GameObject cellObj = Instantiate(cellPrefab, pos, transform.rotation);
                 cells[x, y] = cellObj.GetComponent<cell>();
+                cells[x, y].x = x;
+                cells[x, y].y = y;
+                cells[x, y].alive = (Random.value < 0.2f);
+               
             }
         }
     }
--- assignments/Emergence/Assets/cell.cs
+++ assignments/Emergence/Assets/cell.cs
@@ -4,16 +4,64 @@ using UnityEngine;
 
 public class cell : MonoBehaviour
 {
+    GameOfLife GOL;
     public bool alive = false;
+    public int x = -1;
+    public int y = -1;
+    public Color aliveC;
+    public Color deadC;
+    public Renderer rend;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject GOLobj = GameObject.Find("GameOfLifeOBJ");
+        GOL = GOLobj.GetComponent<GameOfLife>();
+        rend = gameObject.GetComponentInChildren<Renderer>();
+        UpdateColor();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+  
     }
+
+    public void UpdateColor()
+    {
+        if (alive)
+        {
+            rend.material.color = aliveC;
+        }
+        else
+        {
+            rend.material.color = deadC;
+        }
+    }
+    
+    private void OnMouseDown()
+    {
+        alive = !alive;
+        UpdateColor();
+        //Debug.Log(x + ", " + y + "alive: " + alive);
+        Debug.Log(CountLive());
+    }
+    int CountLive()
+    {
+        int alive = 0;
+        for (int xIndex = x - 1; xIndex <= x+1; xIndex++)
+        {
+            for (int yIndex = y - 1; yIndex <= y + 1; yIndex++)
+            {
+                if(GOL.cells[xIndex, yIndex].alive)
+                {
+                    alive++;
+                }
+            }
+        } 
+
+        return alive;
+    }
+
 }
