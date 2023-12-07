2096346c96eb6134fb7ccdf1c14bb3114495d6ac
Yifan SHI <41503744+AidenSaika@users.noreply.github.com>
2023-12-06 22:00:28 -0500

    .

--- assignments/05_units/Assets/imposter.cs
+++ assignments/05_units/Assets/imposter.cs
@@ -10,7 +10,10 @@ public class imposter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+
         
+
     }
 
     // Update is called once per frame
